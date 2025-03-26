b2a0234ad9174adc73c359000a5f2d729b4127df
hank_lai <hank.lai@nordlinglab.org>
2025-03-26 12:30:18 +0800

    update code

--- test2/FFT_code.py
+++ test2/FFT_code.py
@@ -1,26 +1,33 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# 讀取 rPPG 訊號
-rPPG_data = np.loadtxt("rPPG.txt")  # 假設 TXT 只有 rPPG 數值
-sampling_rate = 30  # 輸入影片的頻率
-N = len(rPPG_data)  # 訊號長度
+# read rPPG signal
+rPPG_data = np.loadtxt("rPPG.txt") # np.loadtxt() is used to read .txt or .csv file
+sampling_rate = 30  # sampling_rate is the frequency of input video
+N = len(rPPG_data)  # len() is the length of signal
 
-# 產生時間軸
-t = np.linspace(0, (N-1) / sampling_rate, N) # np.linspace(start, stop, num)
+# generate time axis
+t = np.linspace(0, (N-1) / sampling_rate, N) # np.linspace(start, stop, num) is a function to generate time x-axis
 
-# 計算 FFT
-fft_result = np.fft.fft(rPPG_data)  # 用於計算傅立葉轉換的公式，主要是將時域信號轉換為頻域信號
-freqs = np.fft.fftfreq(N, d=1 / sampling_rate)  # 計算頻率對應關係，np.fft.fftfreq 是 NumPy 中用來生成傅立葉轉換後對應的頻率軸的函數。
+# calculate FFT
+fft_result = np.fft.fft(rPPG_data)  # np.fft.fft() is a function to translate signal from  time domain into frequency domain
+freqs = np.fft.fftfreq(N, d=1 / sampling_rate)  # np.fft.fftfreq(N, d=1 / sampling_rate) is a function to generate frequency domain x-axis
 
-# 只取正頻率部分
+# only pick positive side
 half_N = N // 2
- 
+freqs = freqs[:half_N]
+fft_magnitude = np.abs(fft_result[:half_N])
 
-# 畫圖
+positive_freqs = freqs > 0  # 過濾出 freq > 0 的索引
+peak_index = np.argmax(fft_magnitude[positive_freqs])  # 找到最大值的索引
+peak_freq = freqs[positive_freqs][peak_index]  # 最大值對應的頻率
+peak_value = fft_magnitude[positive_freqs][peak_index]  # 最大值的振幅
+
+
+# set figure size
 plt.figure(figsize=(12, 5))
 
-# 繪製時域訊號
+# time domain diagram
 plt.subplot(1, 2, 1)
 plt.plot(t, rPPG_data, label="rPPG Signal", color="b")
 plt.xlabel("Time (s)")
@@ -29,13 +36,15 @@ plt.title("Time-Domain Signal")
 plt.legend()
 plt.grid()
 
-# 畫出頻譜圖
+# frequency domain diagram
 plt.subplot(1, 2, 2)
 plt.plot(freqs, fft_magnitude, color="r")
+plt.scatter(peak_freq, peak_value, color="black", marker="o", label="Peak")
+plt.annotate(f"{peak_freq:.2f} Hz", xy=(peak_freq, peak_value), xytext=(peak_freq + 0.5, peak_value),arrowprops=dict(facecolor='black', arrowstyle="->"), fontsize=10)
 plt.xlabel("Frequency (Hz)")
 plt.ylabel("Amplitude")
 plt.title("rPPG Frequency Spectrum")
 plt.grid()
 
-plt.tight_layout()  # 自動調整間距
+plt.tight_layout()  # adjust spacing Automatically
 plt.show()
